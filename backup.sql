48cede227803bd4ab5f2499d1288ad998256e50d
kuraeshka <144653643+kuraeshka@users.noreply.github.com>
2026-03-19 21:05:15 +0300

    Add files via upload

---
+++ backup.sql
@@ -0,0 +1,27 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict c2Z4yJesfHQwGb9rgNjr0SQ8HwQpf1fbvNWKJLJQqbJgui8fWuMEAyMew2qm7Zz
+
+-- Dumped from database version 17.2
+-- Dumped by pg_dump version 18.0
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict c2Z4yJesfHQwGb9rgNjr0SQ8HwQpf1fbvNWKJLJQqbJgui8fWuMEAyMew2qm7Zz
+
